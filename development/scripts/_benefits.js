ae8bf5a9815796b5b9d0710dc31a270a5cc0f4b5
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-12 12:49:16 +0700

    benefits scripts

---
+++ development/scripts/_benefits.js
@@ -0,0 +1,13 @@
+(function($) {
+
+    $('.benefits__tag').on('click', function () {
+        const $this = $(this);
+        if ( ! $this.hasClass('benefits__tag--current') ) {
+            $('.benefits__item--current').removeClass('benefits__item--current');
+            $('.benefits__tag--current').removeClass('benefits__tag--current');
+            $('.benefits__item:eq(' + $this.index() + ')').addClass('benefits__item--current');
+            $this.addClass('benefits__tag--current');
+        }
+    });
+
+})(jQuery);
