ce488eb765599b261febe54b90fedac8339c832c
Anton Shchukin <a.a.shchukin@gmail.com>
2024-11-25 13:44:33 +0700

    Import recent changes made on back-end: 1) Programming of forms 2) Agreements and policies via library (remove .article component) 3) Cookies widget

--- src/global/scripts/scripts.js
+++ src/global/scripts/scripts.js
@@ -104,26 +104,38 @@
 
     /* Contacts */
 
-    $('.contact__submit').on('click', function (event) {
+    $('.contact__submit').on('click', async function (event) {
         event.preventDefault();
         const $this = $(this);
         const $form = $this.parents('.contact');
         const $inputs = $form.find('.input__widget');
-        const $alert = Math.random() < 0.5 ? $form.siblings('.inner-alert--success') : $form.siblings('.inner-alert--danger');
+
         let formWidth = $form.outerWidth();
         let formHeight = $form.outerHeight();
 
         if ( !$this.hasClass('button--loading') ) {
             $this.addClass('button--loading');
             $inputs.attr('disabled', true);
-            setTimeout(function () {
-                $this.removeClass('button--loading');
-                $inputs.attr('disabled', false);
-                $form.hide();
-                $alert.show();
-                $alert.css('width', formWidth);
-                $alert.css('height', formHeight);
-            }, 1500);
+
+            const form = new FormData();
+
+            [...$inputs].forEach((input) => {
+	            form.append(input.name, input.value);
+            });
+
+            const response = await fetch('send.php', { method: 'POST', body: form })
+			const result = await response.json();
+
+			const $alert = result.status === 'success'
+        		? $form.siblings('.inner-alert--success')
+				: $form.siblings('.inner-alert--danger');
+
+			$this.removeClass('button--loading');
+            $inputs.attr('disabled', false);
+            $form.hide();
+            $alert.show();
+            $alert.css('width', formWidth);
+            $alert.css('height', formHeight);
         }
     });
 
@@ -136,5 +148,6 @@
         $('.contact').show();
         $('.inner-alert').hide();
     });
+
 
 })(jQuery);
