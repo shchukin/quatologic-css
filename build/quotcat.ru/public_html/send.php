b9b044177a52d125ec7939b5938991b20e7d12cf
Anton Shchukin <a.a.shchukin@gmail.com>
2024-11-26 20:40:59 +0700

    build

--- build/quotcat.ru/public_html/send.php
+++ build/quotcat.ru/public_html/send.php
@@ -3,21 +3,21 @@ header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// Email configuration
-	$to = 'mail@quotalogic.io';
-	$subject = 'Email from quotalogic.io';
+	$to = 'mail@quotcat.ru';
+	$subject = 'Письмо от quotcat.ru';
 	$message = '';
-	
+
 	// Add POST data to email message
     foreach ($_POST as $key => $value) {
         $message .= $key . ': ' . $value . PHP_EOL;
     }
-	
+
 	// Send the email
 	if(mail($to, $subject, $message)) {
-		$response = array('status' => 'success', 'message' => 'Email sent successfully!');
+		$response = array('status' => 'success', 'message' => 'Письмо успешно отправлено!');
 	} else {
 	    http_response_code(500);
-	    $response = array('status' => 'error', 'message' => 'Error sending email: ' . $mail->ErrorInfo);
+	    $response = array('status' => 'error', 'message' => 'Ошибка отправки: ' . $mail->ErrorInfo);
 	}
 } else {
     http_response_code(405);
